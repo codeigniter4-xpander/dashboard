8e83a5faab1d1db5ac5feb5a67656a73ea6ee15a
Bagus A Priambada <iam.adty@gmail.com>
2020-06-08 16:04:58 +0700

    try dashboard

--- src/Controllers/Dashboard.php
+++ src/Controllers/Dashboard.php
@@ -8,15 +8,21 @@ class Dashboard extends \CI4Xpander\Controller
     protected function _init()
     {
         $this->view->data->user->name = \Config\Services::session()->get('user');
+        $this->view->data->template->menu->items[] = new \CI4Xpander_AdminLTE\View\Component\Menu\Item\Data([
+            'name' => 'Dasboard',
+            'url' => 'dashboard',
+            'isActive' => true,
+            'icon' => 'fa fa-dashboard'
+        ]);
     }
 
     public function index()
     {
         return $this->_render(function () {
-            $card = \CI4Xpander_AdminLTE\View\Component\Card::create();
-            $card->data->title = 'CARD';
+            $box = \CI4Xpander_AdminLTE\View\Component\Box::create();
+            $box->data->header->title = 'BOX';
 
-            $this->view->data->template->content = $card->render();
+            $this->view->data->template->content = $box->render();
 
             return $this->view->render();
         });

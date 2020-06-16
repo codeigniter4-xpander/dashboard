04202de81f022b5ec267120edfd0c7b4fd917ac1
Bagus A Priambada <iam.adty@gmail.com>
2020-06-16 13:54:46 +0700

    basic CRUD : read index

--- src/Language/en/CI4Xpander_Dashboard.php
+++ src/Language/en/CI4Xpander_Dashboard.php
@@ -1,5 +1,20 @@
 <?php
 
 return [
-    'indexPageTitle' => '{0} List'
+    'general' => [
+        'status' => [
+            'active' => 'Active',
+            'inactive' => 'Inactive',
+        ],
+        'action' => 'Action',
+        'detail' => 'Detail',
+        'update' => 'Update',
+        'delete' => 'Delete',
+        'create' => 'Create',
+    ],
+    'index' => [
+        'page' => [
+            'title' => '{0} List',
+        ],
+    ],
 ];

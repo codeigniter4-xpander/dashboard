4d0ff6dfd0c040def049db2c4fd90e3e645b2f8c
Bagus A Priambada <iam.adty@gmail.com>
2020-07-15 20:32:08 +0700

    dashboard/setting/user crud index

--- src/Controllers/Dashboard/Setting/User.php
+++ src/Controllers/Dashboard/Setting/User.php
@@ -10,6 +10,21 @@ class User extends \CI4Xpander_Dashboard\Controller
 
         $this->CRUD = [
             'enable' => true,
+            'base_url' => base_url('dashboard/setting/user'),
+            'permission' => 'dashboardSettingUser',
+            'index' => [
+                'isDataTable' => true,
+                'isServerSide' => true,
+                'isMapResultServerSide' => true,
+                'query' => \Config\Database::connect()->table('user u')
+                    ->join('status s1', 's1.id = u.status_id')
+                    ->where('u.deleted_at', null)
+                    ->where('s1.code', 'active'),
+                'columns' => [
+                    'name' => 'Name',
+                    'email' => 'Email'
+                ],
+            ],
         ];
     }
 }

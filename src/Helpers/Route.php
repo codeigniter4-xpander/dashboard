5e26682bbed8d9a34a84e5fdaa182a14e7776bb9
Bagus A Priambada <iam.adty@gmail.com>
2020-07-28 20:22:44 +0700

    route create helper

---
+++ src/Helpers/Route.php
@@ -0,0 +1,13 @@
+<?php namespace CI4Xpander_Dashboard\Helpers;
+
+class Route
+{
+    public static function create(\CodeIgniter\Router\RouteCollection $routes, $namespace = '')
+    {
+        $routes->get('/', "{$namespace}::index");
+		$routes->get('data', "{$namespace}::data");
+		$routes->match(['get', 'post'], 'create', "{$namespace}::create");
+		$routes->match(['get', 'post'], 'update/(:num)', "{$namespace}::update/$1");
+		$routes->post('delete/(:num)', "{$namespace}::delete/$1");
+    }
+}

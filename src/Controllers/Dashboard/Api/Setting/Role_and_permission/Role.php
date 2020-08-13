6beb9f2bdca6fae72cddbf4726388fcd8e36bf2f
Bagus A Priambada <iam.adty@gmail.com>
2020-08-13 16:15:56 +0700

    user create function & CRUD update form

--- src/Controllers/Dashboard/Api/Setting/Role_and_permission/Role.php
+++ src/Controllers/Dashboard/Api/Setting/Role_and_permission/Role.php
@@ -1,5 +1,7 @@
 <?php namespace CI4Xpander_Dashboard\Controllers\Dashboard\Api\Setting\Role_and_permission;
 
+use CI4Xpander\Helpers\Input;
+
 class Role extends \CI4Xpander_API\Controller
 {
     protected $_name = 'Role';
@@ -8,12 +10,22 @@ class Role extends \CI4Xpander_API\Controller
     {
         parent::_init();
 
+        $query = \Config\Database::connect()->table('role')
+            ->select('role.*')
+            ->select('role.name text');
+
+        $input = Input::filter($this->request->getGet());
+        if (array_key_exists('where_not', $input)) {
+            $split = explode('.', $input['where_not']);
+            $kolom = $split[0];
+            $value = $split[1];
+            $query->where("{$kolom} !=", $value);
+        }
+
         $this->setCRUD([
             'enable' => true,
             'index' => [
-                'query' => \Config\Database::connect()->table('role')
-                    ->select('role.*')
-                    ->select('role.name text'),
+                'query' => $query,
                 'searchColumns' => [
                     'name',
                 ],

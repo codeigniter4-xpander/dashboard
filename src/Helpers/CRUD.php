38223999b8c9bdae62cebe46fe0d029d2fc41e4f
Bagus A Priambada <iam.adty@gmail.com>
2020-10-08 18:07:01 +0700

    render field if label value is array

--- src/Helpers/CRUD.php
+++ src/Helpers/CRUD.php
@@ -7,6 +7,16 @@ class CRUD
         if (is_array($label)) {
             if (is_callable($label['value'])) {
                 return $label['value'](is_object($data) ? $data->{$name} : $data[$name], $data);
+            } if (is_array($label['value'])) {
+                $subValue = '<ul>';
+                foreach ($label['value'] as $subName => $subLabel) {
+                    if (is_object($data)) {
+                        $subValue .= "<li>{$data->{$subName}}</li>";
+                    } else {
+                        $subValue .= "<Li>{$data[$subName]}</li>";
+                    }
+                }
+                return $subValue . '</ul>';
             } else {
                 if (is_object($data)) {
                     return $data->{$label['value']};

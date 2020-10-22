c0c0e21e0eff407cda4396834ef7f24d7846e456
Bagus A Priambada <iam.adty@gmail.com>
2020-10-22 10:13:32 +0700

    use core message service

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -1,13 +1,4 @@
 <?php namespace CI4Xpander_Dashboard\Config;
 
 class Services extends \CodeIgniter\Config\BaseService
-{
-    public static function dashboardMessage($type = null, $value = null, bool $shared = true)
-    {
-        if ($shared) {
-            return static::getSharedInstance('dashboardMessage', $type, $value);
-        }
-
-        return \CI4Xpander_Dashboard\Helpers\Message::create($type, $value);
-    }
-}
+{}
